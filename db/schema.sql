c7e33e6d70c77fa12b048cfa2d861bf870ff20b2
J Pennington <grendelsdj@gmail.com>
2018-05-15 18:19:41 -0500

    schema.db created

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,54 @@
 -- creating database -- 
-DROP DATABASE IF EXISTS  burger_db;
-CREATE DATABASE burger_db;
+DROP DATABASE IF EXISTS  staffd_db;
+CREATE DATABASE staffd_db;
 
-USE burger_db;
+USE staffd_db;
 
-CREATE TABLE burgers (
-    id INTEGER(10) AUTO_INCREMENT NOT NULL,
+CREATE TABLE talent (
+    talentId INT (10) AUTO_INCREMENT NOT NULL,
     name VARCHAR (45) NOT NULL,
-    devoured BOOLEAN,
-    PRIMARY KEY(id),
-    createdAt VARCHAR(45),
-    updatedAt VARCHAR(45)
+    phone INT NOT NULL,
+    email VARCHAR (50) NOT NULL,
+    address TINYTEXT NOT NULL,
+    socSec INT NOT NULL,
+    driverLic BOOLEAN,
+    driverLicNum INT,
+    tabc BOOLEAN
+    tabcNum INT,
+    photo BLOB NOT NULL,
+    otherFiles BLOB,
+    type SET ('Bartender', 'Server', 'Promo Model', 'Security', 'Sales') NOT NULL,
+    bio TEXT,
+    rating INT,
+    invitationsAvail VARCHAR(300),
+    currentEvents VARCHAR(300),
+    previousEvents VARCHAR(300),
+    PRIMARY KEY(id)
+
+);
+
+CREATE TABLE event_owners (
+    eventOwnerId INT (10) AUTO_INCREMENT NOT NULL,
+    eventOwner VARCHAR (45) NOT NULL,
+    phone INT NOT NULL,
+    email VARCHAR (50) NOT NULL,
+    address TINYTEXT NOT NULL,
+    photo BLOB NOT NULL,
+    eventsOwned VARCHAR (300),
+    invitationsSent VARCHAR (300),
+    invConfirmed VARCHAR (300)
+);
+
+CREATE TABLE events (
+    eventId INT (10) AUTO_INCREMENT NOT NULL,
+    eventOwner VARCHAR (45) NOT NULL,
+    eventDateTimeStart DATETIME,
+    eventDateTimeEnd DATETIME,
+    locationAddress TINYTEXT NOT NULL,
+    locationSpecialInst MEDIUMTEXT,
+    onsiteContact INT,
+    staffd BOOLEAN,
+    invitationsSent VARCHAR (300),
+    invConfirmed VARCHAR (300)
+
 );
