ea3863bbebd0e72751b52a0d3983325c1e1e788d
JBD <Duck126@hotmail.com>
2018-05-16 16:04:05 -0500

    Schema fix

--- controllers/staffd_controller.js
+++ controllers/staffd_controller.js
@@ -8,10 +8,14 @@ var router = express.Router();
 
 
 // Create all our routes and set up logic within those routes where required.
-router.get("/", function(req, res) {
-    res.render("index");
-    console.log(req.body);
-    console.log(res);
+router.get("/userProfile", function(req, res) {
+    res.render("userProfile");
+    //console.log(req.body);
+    //console.log(res);
 });
+
+router.get("/index", function(req, res) {
+    res.render("index");
+})
 
 module.exports = router;
