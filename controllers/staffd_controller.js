44bbe980de13d86298c4286f84ae8ab51518e00c
JBD <Duck126@hotmail.com>
2018-05-18 01:03:09 -0500

    Homepage styles, Routes through sign up

--- controllers/staffd_controller.js
+++ controllers/staffd_controller.js
@@ -5,8 +5,8 @@ var router = express.Router();
 var db = require("../models/index.js");
 
 // Create all our routes and set up logic within those routes where required.
-router.get("/", function (req, res) {
-    res.render("signin");
+router.get("/signin", function (req, res) {
+    res.render("signin", {layout:'homemain.handlebars'});
 });
 
 router.get("/userProfile", function(req, res) {
@@ -17,16 +17,16 @@ router.get("/index", function(req, res) {
     res.render("index");
 });
 
-router.get("/home", function (req, res) {
-    res.render("home");
+router.get("/", function (req, res) {
+    res.render("home", {layout:'homemain.handlebars'});
 });
 
 router.get("/talentSignup", function (req, res) {
-    res.render("talentSignup");
+    res.render("talentSignup", {layout: 'signinmain.handlebars'});
 });
 
 router.get("/compSignup", function (req, res) {
-    res.render("compSignup");
+    res.render("compSignup", {layout: 'signinmain.handlebars'});
 });
 
 router.get("/hire", function (req, res) {
@@ -43,6 +43,10 @@ router.get("/eventList", function (req, res) {
 
 router.get("/contact", function (req, res) {
     res.render("contact");
+});
+
+router.get("/signupChoose", function (req, res) {
+    res.render("signupChoose", {layout: 'homemain.handlebars'});
 });
 
 module.exports = router;
