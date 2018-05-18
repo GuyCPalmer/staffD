9cc9e052a6bafd44ebe8bcfa1468f411742c816a
J Pennington <grendelsdj@gmail.com>
2018-05-17 19:15:10 -0500

    still adding stuff

--- server.js
+++ server.js
@@ -1,31 +1,58 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var path = require('path');
-
-var PORT = process.env.PORT || 3000;
-
+var express = require('express');
+var exphbs = require('express-handlebars');
 var app = express();
+var passport = require('passport');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var env = require('dotenv').load();
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(path.join(__dirname, '/public')));
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// parse application/json
+//For BodyParser
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Set Handlebars.
-var exphbs = require("express-handlebars");
+// For Passport
+app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+//For Handlebars
+//app.set('views', './views');
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+/*app.get('/', function (req, res) {
+    res.send('Welcome to Passport with Sequelize');
+});*/
+
+//Static Files
+app.use(express.static(__dirname + "/public"));
+
+//Models
+var models = require("./models");
+
+//Routes
+var authRoute = require('./routes/auth.js')(app, passport);
+
+//load passport strategies
+require('./config/passport/passport.js')(passport, models.user);
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/burgers_controller.js");
+var routes = require("./controllers/staffd_controller.js");
 
-app.use( "/", routes);
+app.use("/", routes);
+//Sync Database
+models.sequelize.sync().then(function () {
+    console.log('Nice! Database looks fine');
+}).catch(function (err) {
+    console.log(err, "Something went wrong with the Database Update!");
+});
 
-app.listen(PORT, function() {
-  console.log("App now listening at localhost:" + PORT);
+app.listen(5000, function (err) {
+    if (!err) {
+        console.log("Site is live at 5000");
+    } else {
+        console.log(err);
+    }
+
 });
