4628e8bb9b845c2e6130576e95be2428ef20b4cd
Pierce <piercemorrill91@gmail.com>
2018-05-18 17:43:29 -0500

    consolidating

--- public/assets/js/compSignup.js
+++ public/assets/js/compSignup.js
@@ -1,32 +1,31 @@
 $(document).ready(function(){
 
-    console.log('ugawewlfhawluifhahgaijgokdgpoiadg;joshgjphpgo;ugawewlfhawluifhahgaijgokdgpoiadgugawewlfhawluifhahgaijgokdgpoiadgugawewlfhawluifhahgaijgokdgpoiadgugawewlfhawluifhahgaijgokdgpoiadgugawewlfhawluifhahgaijgokdgpoiadgugawewlfhawluifhahgaijgokdgpoiadgapd;hgj');
 //inserting in for the new data fields
-var newCompany = $('#newCompany');
-var coordinatorName = $("#coordinatorName");
-var purchasing = $('#purchasing');
-var companyPhone = $('#companyPhone');
-var comEmail = $('#comEmail');
-// var comPic = $('#comPic');
-var comAddress = $('#comAddress');
+    var newCompany = $('#newCompany');
+    var coordinatorName = $("#coordinatorName");
+    var purchasing = $('#purchasing');
+    var companyPhone = $('#companyPhone');
+    var comEmail = $('#comEmail');
+    // var comPic = $('#comPic');
+    var comAddress = $('#comAddress');
 
-console.log("SHUTSHUTSHUTSUHSTHUSTUHST");
+    console.log("SHUTSHUTSHUTSUHSTHUSTUHST");
 
 
-function insertComp(){
-event.preventDefault();
-var event_owners = {
-    eventOwner: newCompany.val().trim(),
-    phone: companyPhone.va().trim(),
-    email: comEmail.val().trim(),
-    coordinator: coordinatorName.val().trim(),
-    ownerAddress: comAddress.val().trim()
-    };
+    function insertComp(){
+    event.preventDefault();
+    var event_owners = {
+        eventOwner: newCompany.val().trim(),
+        phone: companyPhone.va().trim(),
+        email: comEmail.val().trim(),
+        coordinator: coordinatorName.val().trim(),
+        ownerAddress: comAddress.val().trim()
+        };
 
-    $.post('/api/eventOwners', event_owners);
+        $.post('/api/eventOwners', event_owners);
 
-    console.log(req.body);
-}//
+        console.log(req.body);
+    }//
 
 });//end of document.ready
 
---
+++ public/assets/js/talentSignup.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+    var newTalent = $('#newTalent');
+    var talentPhone = $('#talentPhone');
+    var talentEmail = $('#talentEmail');
+    var address = $('#address');
+    var SSN = $('#SSN');
+    var dlNumber = $('#dlNumber');
+    var tabc = $('#tabc');
+    
+
+});//endd of doccument
