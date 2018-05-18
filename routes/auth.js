ca796237e1eaa4555a286a32092922f31d0f6565
Pierce <piercemorrill91@gmail.com>
2018-05-18 13:01:20 -0500

    commited out Google auth, Passport, and 3rd auth file

--- routes/auth.js
+++ routes/auth.js
@@ -1,21 +1,21 @@
-const GoogleStrategy = require('passport-google-oauth')
-    .OAuth2Strategy;
+// const GoogleStrategy = require('passport-google-oauth')
+//     .OAuth2Strategy;
 
-module.exports = function (passport) {
-    passport.serializeUser((user, done) => {
-        done(null, user);
-    });
-    passport.deserializeUser((user, done) => {
-        done(null, user);
-    });
-    passport.use(new GoogleStrategy({
-        clientID: "887616308420-ddtpflmikdpebomcg3mvj5uvbctg941f.apps.googleusercontent.com", //need to add google auth clientID
-        clientSecret: "g06mcVdwHyY0P0qO7pEOMeAG", // need to add google auth clientSecret
-        callbackURL: '/auth/google/callback'
-    }, (token, refreshToken, profile, done) => {
-        return done(null, {
-            profile: profile,
-            token: token
-        });
-    }));
-};
+// module.exports = function (passport) {
+//     passport.serializeUser((user, done) => {
+//         done(null, user);
+//     });
+//     passport.deserializeUser((user, done) => {
+//         done(null, user);
+//     });
+//     passport.use(new GoogleStrategy({
+//         clientID: "887616308420-ddtpflmikdpebomcg3mvj5uvbctg941f.apps.googleusercontent.com", //need to add google auth clientID
+//         clientSecret: "g06mcVdwHyY0P0qO7pEOMeAG", // need to add google auth clientSecret
+//         callbackURL: '/auth/google/callback'
+//     }, (token, refreshToken, profile, done) => {
+//         return done(null, {
+//             profile: profile,
+//             token: token
+//         });
+//     }));
+// };
