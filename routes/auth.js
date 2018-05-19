6ab02a484dca2b80d5be7e2932b962d299b8ea63
JBD <Duck126@hotmail.com>
2018-05-19 12:38:02 -0500

    Hire Display Talent

--- routes/auth.js
+++ routes/auth.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-// const GoogleStrategy = require('passport-google-oauth')
-//     .OAuth2Strategy;
-
-// module.exports = function (passport) {
-//     passport.serializeUser((user, done) => {
-//         done(null, user);
-//     });
-//     passport.deserializeUser((user, done) => {
-//         done(null, user);
-//     });
-//     passport.use(new GoogleStrategy({
-//         clientID: "887616308420-ddtpflmikdpebomcg3mvj5uvbctg941f.apps.googleusercontent.com", //need to add google auth clientID
-//         clientSecret: "g06mcVdwHyY0P0qO7pEOMeAG", // need to add google auth clientSecret
-//         callbackURL: '/auth/google/callback'
-//     }, (token, refreshToken, profile, done) => {
-//         return done(null, {
-//             profile: profile,
-//             token: token
-//         });
-//     }));
-// };
-=======
 var authController = require('../controllers/authcontroller.js');
 
 module.exports = function(app,passport){
@@ -49,4 +26,3 @@ function isLoggedIn(req, res, next) {
 
     res.redirect('/signin');
 }};
->>>>>>> 088b98e5ad17de1b03081fa8b7de74743fdaa14f
