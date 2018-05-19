fff050a7a26fb268a6615829d9c4c45aaa9c0c62
Guy Palmer <guypalmer@Guys-MacBook-Air.local>
2018-05-19 12:25:52 -0500

    I see no changes, I wake up in the morning and I ask myself

--- public/assets/js/rating.js
+++ public/assets/js/rating.js
@@ -1,48 +1,21 @@
-function StarRating() {
-    this.init();
-  };
-   
-  /**
-   * Initialize
-   */
-  StarRating.prototype.init = function() {
-    this.stars = document.querySelectorAll('#rating span');
-    for (var i = 0; i < this.stars.length; i++) {
-      this.stars[i].setAttribute('data-count', i);
-      this.stars[i].addEventListener('mouseenter', this.enterStarListener.bind(this));
+var $star_rating = $('.star-rating .fa');
+
+var SetRatingStar = function() {
+  return $star_rating.each(function() {
+    if (parseInt($star_rating.siblings('input.rating-value').val()) >= parseInt($(this).data('rating'))) {
+      return $(this).removeClass('fa-star-o').addClass('fa-star');
+    } else {
+      return $(this).removeClass('fa-star').addClass('fa-star-o');
     }
-    document.querySelector('#rating').addEventListener('mouseleave', this.leaveStarListener.bind(this));
-  };
-   
-  /**
-   * This method is fired when a user hovers over a single star
-   * @param e
-   */
-  StarRating.prototype.enterStarListener = function(e) {
-    this.fillStarsUpToElement(e.target);
-  };
-   
-  /**
-   * This method is fired when the user leaves the #rating element, effectively removing all hover states.
-   */
-  StarRating.prototype.leaveStarListener = function() {
-    this.fillStarsUpToElement(null);
-  };
-   
-  /**
-   * Fill the star ratings up to a specific position.
-   * @param el
-   */
-  StarRating.prototype.fillStarsUpToElement = function(el) {
-    // Remove all hover states:
-    for (var i = 0; i < this.stars.length; i++) {
-      if (el == null || this.stars[i].getAttribute('data-count') > el.getAttribute('data-count')) {
-        this.stars[i].classList.remove('hover');
-      } else {
-        this.stars[i].classList.add('hover');
-      }
-    }
-  };
-   
-  // Run:
-  new StarRating();
+  });
+};
+
+$star_rating.on('click', function() {
+  $star_rating.siblings('input.rating-value').val($(this).data('rating'));
+  return SetRatingStar();
+});
+
+SetRatingStar();
+$(document).ready(function() {
+
+});
