f17f08bc98eceefd08afaf20260ce4da6f32fb50
Guy Palmer <guypalmer@Guys-Air.attlocal.net>
2018-05-22 09:52:30 -0500

    ok

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -21,8 +21,4 @@ exports.logout = function(req,res){
   req.session.destroy(function(err) {
   res.redirect('/');
   });
-<<<<<<< HEAD
-=======
-
->>>>>>> 1a593a8cc58a9918db13c6322f83be692bd5d602
 };
