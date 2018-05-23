f0b7fbc3d0c702ff6c790bf6f9d4961660a6e46a
Pierce Morrill <piercemorrill91@gmail.com>
2018-05-22 20:23:52 -0500

    fixed contact/userprofile

--- public/assets/js/eventList.js
+++ public/assets/js/eventList.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     function getEvents() {
         $.get("/api/events/", function (req, res, eventData) {
             console.log(eventData, "front end event data");
-            res.json(eventData);
+
         }).then(function(err, eventData){
             if(err){
                 console.log(err, "eventList.js ERR");
